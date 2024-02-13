9c9d35b45407239fa231adbb77eee74a5edfc115
Zur <yarden7671@gmail.com>
2024-02-13 23:00:32 +0200

    commit all

---
+++ app/src/main/java/com/example/cardmodel2/CardModel.java
@@ -0,0 +1,28 @@
+package com.example.cardmodel2;
+
+public class CardModel
+{
+    String text;
+    int image;
+
+    public CardModel(String text, int image)
+    {
+        this.text=text;
+        this.image=image;
+    }
+    public String GetText()
+    {
+        return text;
+    }
+    public int GetImage()
+    {
+        return image;
+    }
+
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/cardmodel2/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.cardmodel2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    int[] images = {R.drawable.icon1,R.drawable.icon2,
+            R.drawable.icon3,R.drawable.icon4,R.drawable.icon5,R.drawable.icon6,
+            R.drawable.icon7,R.drawable.icon8,R.drawable.icon9,R.drawable.icon10,
+            R.drawable.icon11,R.drawable.icon12,R.drawable.icon13,R.drawable.icon14,
+            R.drawable.icon15,R.drawable.icon16,R.drawable.icon17,R.drawable.icon18,
+            R.drawable.icon19,R.drawable.icon20};
+
+    ArrayList<CardModel> models = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void setUpModels(){
+        String[]texts= getResources().getStringArray(R.array.numbers);
+        for(int i=0; i<texts.length; i++){
+            models.add(new CardModel(texts[i],images[i]));
+        }
+    }
+
+}
